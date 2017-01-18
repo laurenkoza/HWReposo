0180890d950b58f95b63e5be7589d0c625900cae
Koza <l_koza3@tss-hr420-dm01.fc.ca>
2017-01-18 13:39:54 -0500

    jquery conversion made, pushing now

--- script/main.js
+++ script/main.js
@@ -3,12 +3,22 @@
 //need images, heading, subheading, season text, appliedClass
 	var images;
 
-var theImages = document.querySelectorAll(".image-holder"),
-	heading = document.querySelector(".heading"),
-	subheading = document.querySelector(".main-copy h2"),
-	paragraph = document.querySelector(".main-copy p"),
+/* Vanilla JS
+	var theImages = document.querySelectorAll(".image-holder"),
+		heading = document.querySelector(".heading"),
+		subheading = document.querySelector(".main-copy h2"),
+		paragraph = document.querySelector(".main-copy p"),
+		appliedClass;
+		*/
+
+//jQuery
+var $theImages = $(".image-holder"),
+	$heading = $('.heading'),
+	$subheading = $('.main-copy h2'),
+	$paragraph = $('.main-copy p'),
 	appliedClass;
 
+/* Vanilla JS
 	function changeElements(){
 		heading.classList.remove(appliedClass); //so the colours don't get stuck, removes previous styling
 		subheading.classList.remove(appliedClass);
@@ -21,14 +31,38 @@ var theImages = document.querySelectorAll(".image-holder"),
 		subheading.firstChild.nodeValue = dynamicContent[this.id].headline; //dynamic content is the name of the object
 		paragraph.firstChild.nodeValue = dynamicContent[this.id].text;
 	}
+*/
 
+//jQuery
+	function changeElements(){
+		$heading.removeClass(appliedClass); //so the colours don't get stuck, removes previous styling
+		$subheading.removeClass(appliedClass);
+		//debugger;
+		appliedClass = event.currentTarget.id;
+
+		$heading.addClass(event.currentTarget.id);
+		$subheading.addClass(event.currentTarget.id);
+		
+		$subheading.text(dynamicContent[event.currentTarget.id].headline); //dynamic content is the name of the object
+		$paragraph.text(dynamicContent[event.currentTarget.id].text);
+	}
+
+
+	$theImages.click(function(){ //jQuery event listener, on click run this function
+		//console.log("clicked!");
+		changeElements();
+	});
+
+
+/* Vanilla JS
 	[].forEach.call(theImages, function(image){ //for each image variable, run this array/ function
 		image.addEventListener('click', changeElements, true);
 	});
+*/
 
-	subheading.firstChild.nodeValue = dynamicContent['spring'].headline; //changing placeholder text on load
-	paragraph.firstChild.nodeValue = dynamicContent['spring'].text;
-	heading.classList.add('spring');
+	$subheading.text(dynamicContent['spring'].headline); //changing placeholder text
+	$paragraph.text(dynamicContent['spring'].text);
+	$heading.addClass('spring');
 
 
 })();
